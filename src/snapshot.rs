5f6f8bb95bb850e198f907029b5614925a017f86
Jacob Turner <jacob.turner870@gmail.com>
2026-07-24 13:44:49 +0200

    Added licensing

--- src/snapshot.rs
+++ src/snapshot.rs
@@ -1,3 +1,6 @@
+// Copyright (C) 2026, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
 //! `snapshot` subcommand with macro preprocessing.
 //!
 //! This is a reimplementation of Foundry's `forge snapshot` command
